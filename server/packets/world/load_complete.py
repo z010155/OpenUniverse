eee6ddcd021ad7c8014bd77e954c9cbe9d42c4f3
noud02 <noud.kerver@hotmail.nl>
2018-06-10 15:23:32 +0200

    fix clientside load complete packet

--- server/packets/world/load_complete.py
+++ server/packets/world/load_complete.py
@@ -1,9 +1,13 @@
 from pyraknet.bitstream import WriteStream, ReadStream, c_uint16, c_uint32
 
+from server.enums import ConnectionType
 from server.packet import Packet
 
 
 class ClientsideLoadCompletePacket(Packet):
+    connection_type = ConnectionType.Client
+    packet_id = 0x13
+
     zone_id: int
     instance: int
     clone: int
